0c78be4b2b66e9fa6271c4c3df2a2932b1f276d4
ouki.wang <ouki.wang@dfrobot.com>
2019-05-25 13:45:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# gamePad
+
+Micro:bit game handle expansion board.
+![image](http://wiki.dfrobot.com.cn/images/9/98/DFR0536%E5%A4%96%E5%BD%A2%E5%9B%BE.png)
+---------------------------------------------------------
+
+## Table of Contents
+
+* [URL](#url)
+* [Summary](#summary)
+* [Blocks](#blocks)
+* [License](#license)
+
+## URL
+project URL:  ```https://github.com/DFRobot/pxt-gamePad```
+
+## Summary
+
+Micro: the bit game handle is based on micro: the extension handle of the bit main board.All you need to do is plug in micro: the bit board, and you can turn micro:bit into a wireless remote control handle or a wireless game console.The product is made of acrylic sheet, and it feels good. It will not have the feel of sticking hands like exposed circuit board.
+There are 8 buttons in the game handle, and the left hand side is divided into the top, bottom, left and right buttons. The right side is X and Y, and the front leads to the two buttons of A and B.
+This game controller also has built-in vibration motor, buzzer, LED, these resources are programmable control.Using graphical programming, you can instantly turn your micro:bit into a multimedia vibrator or multimedia interactive console.
+The programming platform supports makecode graphical programming and python programming, which is a great tool for programming from entry to advanced programming education.
+
+## Blocks
+
+### 1.LED control
+![image](https://github.com/DFRobot/pxt-gamePad/blob/master/image/1.png)
+
+### 2.Vibration motor control
+![image](https://github.com/DFRobot/pxt-gamePad/blob/master/image/2.png)<br>
+![image](https://github.com/DFRobot/pxt-gamePad/blob/master/image/3.png)
+
+### 3.Buttons are triggered as events.
+![image](https://github.com/DFRobot/pxt-gamePad/blob/master/image/4.png)
+
+### 4.Check button status
+![image](https://github.com/DFRobot/pxt-gamePad/blob/master/image/5.png)
+
+
+## License
+
+GNU
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+```package
+gamePad=github:DFRobot/pxt-gamePad
+```
---
+++ keyboard.ts
@@ -0,0 +1,171 @@
+/*！
+ * @file gamePad/main.ts
+ * @brief DFRobot's gamer pad makecode library.
+ * @n [Get the module here](http://www.dfrobot.com.cn/goods-1577.html)
+ * @n This is the microbit dedicated handle library, which provides an API to 
+ * control eight buttons, including an led indicator light and a vibrating motor.
+ *
+ * @copyright	[DFRobot](http://www.dfrobot.com), 2016
+ * @copyright	GNU Lesser General Public License
+ *
+ * @author [email](1035868977@qq.com)
+ * @version  V1.0
+ * @date  2018-03-20
+ */
+
+/**
+ * User Buttons for DFRobot gamer:bit Players.
+ */
+//%
+enum GamerBitPin {
+    //% block="X button"
+    P1 = <number>DAL.MICROBIT_ID_IO_P1,
+    //% block="Y button"
+    P2 = <number>DAL.MICROBIT_ID_IO_P2,
+    //% block="D-PAD up"
+    P8 = <number>DAL.MICROBIT_ID_IO_P8,
+    //% block="D-PAD down"
+    P13 = <number>DAL.MICROBIT_ID_IO_P13,
+    //% block="D-PAD left"
+    P14 = <number>DAL.MICROBIT_ID_IO_P14,
+    //% block="D-PAD right"
+    P15 = <number>DAL.MICROBIT_ID_IO_P15,
+}
+
+/**
+ * Trigger Events Proposed by DFRobot gamer:bit Players.
+ */
+//%
+enum GamerBitEvent {
+    //% block="pressed"
+    Down = DAL.MICROBIT_BUTTON_EVT_DOWN,
+    //% block="released"
+    Up = DAL.MICROBIT_BUTTON_EVT_UP,
+    //% block="click"
+    Click = DAL.MICROBIT_BUTTON_EVT_CLICK,
+}
+
+/**
+ * Functions for DFRobot gamer:bit Players.
+ */
+//% weight=10 color=#DF6721 icon="\uf11b" block="keyboard"
+namespace keyboard {
+    let PIN_INIT = 0;
+    
+    export enum Vibrator { 
+        //% blockId="V0" block="stop"
+        V0 = 0,
+        //% blockId="V1" block="Vibration"
+        V1 = 255,     
+    }
+
+    export enum Intensity { 
+        //% blockId="I0" block="stop"
+        I0 = 0,
+        //% blockId="I1" block="weak"
+        I1 = 100,
+        //% blockId="I2" block="medium"
+        I2 = 180,
+        //% blockId="I3" block="strong"
+        I3 = 225
+    }
+
+    export enum Led {
+        //% blockId="OFF" block="off"
+        OFF = 0,
+        //% blockId="ON" block="on"
+        ON = 1
+    }
+
+
+    //% shim=gamerpad::init
+    function init(): void {
+        return;
+    }
+
+    function PinInit(): void {
+        pins.setPull(DigitalPin.P1, PinPullMode.PullNone);
+        pins.setPull(DigitalPin.P2, PinPullMode.PullNone);
+        pins.setPull(DigitalPin.P8, PinPullMode.PullNone);
+        pins.setPull(DigitalPin.P13, PinPullMode.PullNone);
+        pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
+        pins.setPull(DigitalPin.P15, PinPullMode.PullNone);
+        pins.setPull(DigitalPin.P0, PinPullMode.PullUp);
+        pins.setPull(DigitalPin.P16, PinPullMode.PullUp);
+        PIN_INIT = 1;
+        return;
+    }
+
+    /**
+     * To scan a button whether be triggered : return '1' if pressed; return'0' if not.
+     */
+    //% weight=70
+    //% blockId=gamePad_keyState block="button|%button|is pressed"
+    //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
+    export function keyState(button: GamerBitPin): boolean {
+        if (!PIN_INIT) { 
+            PinInit();
+        }
+        let num = false;
+        if (0 == pins.digitalReadPin(<number>button)) {
+            num = true;
+        }
+        return num;
+    }
+
+    /**
+     * Registers code to run when a DFRobot gamer:bit event is detected.
+     */
+    //% weight=60
+    //% blockGap=50
+    //% blockId=gamePad_onEvent block="on button|%button|is %event"
+    //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
+    //% event.fieldEditor="gridpicker" event.fieldOptions.columns=3
+    export function onEvent(button: GamerBitPin, event: GamerBitEvent, handler: Action) {
+        init();
+        if (!PIN_INIT) { 
+            PinInit();
+        }
+        control.onEvent(<number>button, <number>event, handler); // register handler
+    }
+
+    /**
+     * Vibrating motor switch.
+     */
+    //% weight=50
+    //% blockId=gamePad_vibratorMotor block="Vibrator motor switch|%index|"
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
+    export function vibratorMotor(index: Vibrator): void {
+        vibratorMotorSpeed(<number>index);
+        return;
+    }
+
+    /**
+     * Vibration motor speed setting, adjustable range 0~255.
+     */
+    //% weight=30
+    //% blockGap=50
+    //% blockId=gamePad_vibratorMotorSpeed block="Vibrator motor intensity|%degree"
+    //% degree.min=0 degree.max=255
+    export function vibratorMotorSpeed(degree: number): void {
+        if (!PIN_INIT) { 
+            PinInit();
+        }
+        let num = degree * 4;
+        pins.analogWritePin(AnalogPin.P12, <number>num);
+        return;
+    }
+
+    /**
+     * LED indicator light switch.
+     */
+    //% weight=20
+    //% blockId=gamePad_led block="LED|%index|"
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
+    export function led(index: Led): void {
+        if (!PIN_INIT) { 
+            PinInit();
+        }
+        pins.digitalWritePin(DigitalPin.P16, <number>index);
+    }
+}
